75cd89b4c4bb987fc0aec2a970bb49fa6c3a05bb
lilu <lilu.life@gmail.com>
2013-03-25 16:44:37 +0800

    ruby

--- modules/my-ruby.el
+++ modules/my-ruby.el
@@ -30,6 +30,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.builder\\'" . ruby-mode))
 (require 'haml-mode)
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 (rvm-use-default)
