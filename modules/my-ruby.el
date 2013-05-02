876215ee9fc82e34d80c4b5e728ee4ae0ea7af04
lilu <lilu.life@gmail.com>
2013-05-02 13:29:13 +0800

    change font

--- modules/my-ruby.el
+++ modules/my-ruby.el
@@ -31,6 +31,7 @@
 (add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.builder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . ruby-mode))
 (require 'haml-mode)
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 (rvm-use-default)
